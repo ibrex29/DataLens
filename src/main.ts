84e6f798dc889567bf4b5b46a6167d9150e9cf10
ibrahim muhammad <ibrex29@gmail.com>
2024-09-23 09:22:18 +0100

    CORS

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import * as compression from 'compression';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+
+const corsOptions: CorsOptions = {
+  origin: true, // Allow all origins, or specify a domain like 'https://example.com'
+  methods: ['GET', 'POST', 'OPTIONS'], // Allowed HTTP methods
+  allowedHeaders: ['*'], // Allowed headers
+};
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(compression()); // use compression to improve speed
+  app.enableCors(corsOptions);
+  // Enable CORS with the defined options
+  app.enableCors(corsOptions);
 
   const config = new DocumentBuilder()
     .setTitle('Data Lens example')
@@ -11,9 +23,11 @@ async function bootstrap() {
     .setVersion('1.0')
     .addBearerAuth() 
     .build();
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
 }
+
 bootstrap();
